02a3275a356b9fa109ee910a9a1ab7c004ed337d
whoisririi <ifatiffany@gmail.com>
2026-02-16 11:24:29 +0800

    Functional Resume Download Button

--- src/sections/AboutSection.tsx
+++ src/sections/AboutSection.tsx
@@ -1,10 +1,6 @@
 import { MapPin, Mail, Phone } from "lucide-react";
 import aboutImg from "../assets/images/aboutimg.jpg";
 
-//image part
-const placeholderImage =
-  "https://via.placeholder.com/300x380.png?text=Your+Photo";
-
 const AboutSection = () => {
   return (
     <section className="w-full bg-lightpink px-6 py-20">
@@ -17,7 +13,7 @@ const AboutSection = () => {
           </h1>
 
           {/* DESCRIPTION */}
-          <p className="text-base sm:text-lg text-rusty font-candal leading-relaxed max-w-xl">
+          <p className="text-base sm:text-lg text-rusty font-source font-bold leading-relaxed max-w-xl">
             I am a creative Computer Science student specializing in UI/UX
             design and front-end development. Currently a 4th year student at
             Pampanga State University. I have experience designing responsive
@@ -27,18 +23,22 @@ const AboutSection = () => {
 
           {/* HIGHLIGHT TEXT */}
           <div>
-            <p className="font-source font-bold text-dirtygold text-lg">
+            <p className="font-source font-bold text-dirtygold text-xl">
               Want to get to know me more?
             </p>
-            <p className="font-source text-dirtygold font-bold text-lg">
-              Check this out!
+            <p className="font-source text-dirtygold font-bold text-xl">
+              Download my resume here:
             </p>
           </div>
 
           {/* RESUME BUTTON */}
-          <button className="w-fit px-6 py-2 border border-royalpink text-royalpink rounded-full font-source font-semibold hover:bg-royalpink hover:text-white transition">
+          <a
+            href="/RESUME.pdf"
+            download="Ifa_Dela_Rosa_Resume.pdf"
+            className="w-fit px-6 py-2 border border-royalpink text-royalpink rounded-full font-source font-semibold hover:bg-royalpink hover:text-white transition"
+          >
             Resume
-          </button>
+          </a>
         </div>
 
         {/* RIGHT SIDE - PROFILE CARD */}
